14711e0dfabfe8ec2a033b3ada6a6e7c46593d13
sa'ed ayman <sa467563@gmail.com>
2022-03-04 23:03:40 +0300

    remove config.env

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
-const colors =require('colors')
+const mongoose = require("mongoose");
+const colors = require("colors");
 
-const connectDB = async()=>{
-    try {
-    const conn = await mongoose.connect(`${process.env.MONGOOSE_URI}`)
-    console.log(`Database connected ${conn.connection.host}`.bgBlue.bold)
-    } catch (error) {
-       console.log(error)
-       process.exit(1) 
-    }
-}
-module.exports = connectDB
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`${process.env.MONGOOSE_URI}`);
+    console.log(`Database connected ${conn.connection.host}`.bgBlue.bold);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+module.exports = connectDB;
